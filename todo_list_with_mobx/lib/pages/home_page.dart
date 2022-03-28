9da9e30f76402f081c9f5d71fc19bfedbe0c0bab
wilker123 <josewilkerlucena123@gmail.com>
2022-03-28 16:32:27 -0300

    Created component dialog in project

--- todo_list_with_mobx/lib/pages/home_page.dart
+++ todo_list_with_mobx/lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'item_widget.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -8,6 +10,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  _dialog() {
+    showDialog(
+      context: context,
+      builder: (_) {
+        return AlertDialog(
+          title: Text("Adicionar item"),
+          content: TextField(
+            onChanged: (value) {},
+            decoration: InputDecoration(
+                border: OutlineInputBorder(), labelText: 'Novo item'),
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Salva"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Cancelar"),
+            )
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +44,18 @@ class _HomePageState extends State<HomePage> {
           decoration: InputDecoration(hintText: "Pesquisa..."),
         ),
       ),
-      body: ListView.builder(itemBuilder: itemBuilde,r),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (_, index) {
+          return ItemWidget();
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          _dialog();
+        },
+      ),
     );
   }
 }
