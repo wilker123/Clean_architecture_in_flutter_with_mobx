1d9db996c70433957756287a72f9a92d7aee23a5
wilker123 <josewilkerlucena123@gmail.com>
2022-03-28 18:21:29 -0300

    Created action addItem

--- todo_list_with_mobx/lib/controllers/home_controller.dart
+++ todo_list_with_mobx/lib/controllers/home_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/widgets.dart';
 import 'package:mobx/mobx.dart';
 import 'package:todo_list_with_mobx/models/item_model.dart';
 
@@ -8,4 +9,9 @@ class HomeController {
     ItemModel(title: "Item 2", check: true),
     ItemModel(title: "Item 3", check: false)
   ];
+
+  @action
+  addItem(ItemModel model) {
+    listItems.add(model);
+  }
 }
