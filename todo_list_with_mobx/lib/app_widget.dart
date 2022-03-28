79384bafddfcff8c3ca6dc4bb2800d44f555f4de
wilker123 <josewilkerlucena123@gmail.com>
2022-03-28 16:14:03 -0300

    configuring MaterialApp in project

---
+++ todo_list_with_mobx/lib/app_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'pages/home_page.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
