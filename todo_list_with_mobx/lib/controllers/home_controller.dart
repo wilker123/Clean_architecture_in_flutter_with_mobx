04dea966751f61320d7f0e3da469b92d75388733
wilker123 <josewilkerlucena123@gmail.com>
2022-03-28 17:43:54 -0300

    generate code mobx with build_runner

---
+++ todo_list_with_mobx/lib/controllers/home_controller.dart
@@ -0,0 +1,11 @@
+import 'package:mobx/mobx.dart';
+import 'package:todo_list_with_mobx/models/item_model.dart';
+
+class HomeController {
+  @observable
+  List<ItemModel> listItems = [
+    ItemModel(title: "item 1", check: false),
+    ItemModel(title: "Item 2", check: true),
+    ItemModel(title: "Item 3", check: false)
+  ];
+}
